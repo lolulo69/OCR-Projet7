a45aa52111e4ce683e37fedc8c2b3c55bd3c256c
lolulo69 <lefortlucas1@gmail.com>
2023-01-13 16:39:30 +0100

    Ajout d'un module fetch sur la page Room Ajout du fetch et notamment de l'outil filter qui nous permet de récupérer toutes les infos du produit que l'on souhaite afficher.

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,5 +1,32 @@
+import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+
 function Room() {
-    return <h1>Room</h1>;
+    const { roomId } = useParams();
+
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('../logements.json');
+            const data = await response.json();
+            const roomData = data.filter((item) => item.id === roomId)[0];
+            setData(roomData);
+            setLoading(false);
+        }
+        fetchData();
+    }, [roomId]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <React.Fragment>
+            <h1>{data.title}</h1>
+        </React.Fragment>
+    );
 }
 
 export default Room;
